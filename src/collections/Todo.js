26400b053896633c4964deb02bc7d2f89d4db0ff
harvdt <harviandito20@gmail.com>
2023-10-27 13:43:16 +0700

    feat: add log collection

--- src/collections/Todo.js
+++ src/collections/Todo.js
@@ -1,8 +1,50 @@
+import payload from 'payload';
+
 /** @type {import('payload/types').CollectionConfig} */
 const Todo = {
     slug : "Todo",
+    access: {
+        read : () => true,
+        create : () => true,
+        update : () => true,
+        delete : () => true
+    },
     admin : {
         useAsTitle : "nama"
+    },
+    hooks: {
+        afterOperation: [
+            async (args) => {
+                if (args.operation == "create") {
+                    payload.create({
+                        collection: "Log",
+                        data: {
+                            collectionName: "todo",
+                            action : "create",
+                            timestamp: new Date()
+                        },
+                    });
+                } else if (args.operation == "update") {
+                    payload.create({
+                        collection: "Log",
+                        data: {
+                            collectionName: "todo",
+                            action : "update",
+                            timestamp: new Date()
+                        },
+                    });
+                } else if (args.operation == "delete") {
+                    payload.create({
+                        collection: "Log",
+                        data: {
+                            collectionName: "todo",
+                            action : "delete",
+                            timestamp: new Date()
+                        },
+                    });
+                } 
+            },
+        ],
     },
     fields : [
         {
